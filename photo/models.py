c7588012c4075f50774850f9b8bf21d31751f8ba
IreneMercy <irenemercy700@gmail.com>
2019-10-04 17:36:22 +0300

    create images,location and category class

--- photo/models.py
+++ photo/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Images(models.Model):
+    image_path = models.ImageField()
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
