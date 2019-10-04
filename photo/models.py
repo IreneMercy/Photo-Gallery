ecdc87a4381f6533a892a3828d6a6e914d361f34
IreneMercy <irenemercy700@gmail.com>
2019-10-04 18:21:31 +0300

    add save function

--- photo/models.py
+++ photo/models.py
@@ -1,11 +1,6 @@
 from django.db import models
 
-class Images(models.Model):
-    image_path = models.ImageField()
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=100)
-    location = models.ForeignKey(Location)
-    category = models.ForeignKey(Category)
+
 
 class Location(models.Model):
     name = models.CharField(max_length=30)
@@ -13,9 +8,28 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
+    def save_location(self):
+        self.save()
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.name
+
+    def save_category(self):
+        self.save()
+
+class Images(models.Model):
+    image_path = models.ImageField()
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=100)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+
+    def __str__(self):
+        return self.description
+
+    def save_images(self):
+        self.save()
