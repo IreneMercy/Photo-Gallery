abb594ca0955cda283b3fb304b828a6175373515
IreneMercy <irenemercy700@gmail.com>
2019-10-07 13:37:20 +0300

    add app url

--- gallery/urls.py
+++ gallery/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from photo.views import home
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$',home, name='home'),
     url(r'^photo/', include('photo.urls'))
 ]
