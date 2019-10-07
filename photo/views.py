1f0eee17529a08e55caf7e2bbff832d656270b09
IreneMercy <irenemercy700@gmail.com>
2019-10-07 13:50:39 +0300

    add function to query images by location

--- photo/views.py
+++ photo/views.py
@@ -1,5 +1,41 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from django.templatetags.static import static
+from .models import Images, Location, Category
+from django.db.models.query import QuerySet
 
 def home(request):
-    return render(request, 'home.html')
+    images = Images.objects.all()
+    location = Location.objects.all()
+    context = {
+        "images":images,
+        "location":location,
+    }
+    return render(request, 'home.html', context)
+
+def images(request, images_id):
+    try:
+        images = Images.objects.get(id = images_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"gall/gallery.html", {"images":images})
+
+def search_image(request):
+    if 'images' in request.GET and request.GET['images']:
+        search_term = request.GET["images"]
+        searched_images = Images.search_by_category(search_term)
+        message = f'{search_term}'
+        return render(request, 'gall/search.html', {"message":message, "images":searched_images})
+
+    else:
+        message = "You haven't searched for any image"
+        return render(request, 'gall/search.html', {"message":message})
+
+def display_by_location(request, id):
+    queryset = Location.objects.all()
+    queryset = Images.objects.filter(location__id=id)
+    context = {
+        "location":queryset,
+        "images":queryset,
+    }
+    return render(request, "location.html", context)
